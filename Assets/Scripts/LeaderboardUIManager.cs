cf4f0a761b9add1d92730ca706c10d869ce382e5
NoahAMendoza <NoahMendoza622@gmail.com>
2022-10-06 12:41:19 -0500

    Probably last commit
    Leaderboard is backwards but I don't care that much, core challenge is done and I want to move on to more exciting things.

--- Assets/Scripts/LeaderboardUIManager.cs
+++ Assets/Scripts/LeaderboardUIManager.cs
@@ -2,19 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LeaderboardUIManager : MonoBehaviour
 {
-
-    public List<(string, int)> leaderboardData;
+    private int MaxScores = 5;
+    public Text[] Entries; 
 
     // Start is called before the first frame update
     void Start()
     {
         DataManager.Instance.LoadAllHighestScores();
         Debug.Log("Grabbed the following high scores: " + DataManager.Instance.highScores.highScoresList[0].playerNameToSave);
+        Debug.Log("Number of high scores: " + DataManager.Instance.highScores.highScoresList.Count);
 
+        showScores();
+        
         //TODO Use data from the above log to populate UI
+    }
+
+    private void showScores()
+    {
+        for(int i = 0; (i < MaxScores) && (i < DataManager.Instance.highScores.highScoresList.Count) ; i++)
+        {
+            Entries[i].text = DataManager.Instance.highScores.highScoresList[i].playerNameToSave + " : " + DataManager.Instance.highScores.highScoresList[i].playerScoreToSave;
+        }
     }
 
     // Update is called once per frame
